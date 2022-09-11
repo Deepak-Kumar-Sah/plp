c0b332dcbcb08b71096596aed56d9142f707c08a
Deepak Kumar <deepaksah9998@gmail.com>
2022-09-11 16:31:05 +0530

    server sol1

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -23,7 +23,7 @@ const resetFilters = ()=>{
 }
   const fetchProducts = async ()=>{
     // const res =await fetch('https://dummyjson.com/products/');
-    const res =await fetch('https://fakestoreapi.com/products');
+    const res =await fetch('https://fakestoreapi.com/products',{method:"GET"});
     const data = await res.json();
     // console.log(data);
     setproducts(data);
